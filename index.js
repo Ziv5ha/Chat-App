e2e2f8778183b12b311a477d71ef499a87ed8bf2
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2021-12-02 20:56:19 +0200

    update index with user router, auth middleware and error handler

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 // const RoomsInfo = require('./models/roomInfoModel');
 //// const authRouter = require('./routers/auth');
 const chatRouter = require('./routers/chatRouter');
+const userRouter = require('./routers/userRouter');
+const auth = require('./middleware/authMiddleware');
+const errorHandler = require('./error-handler/error-handler');
 
-// mongoose.connect(process.env.DATABASE).then(() => {
-//   console.log('DB connected');
-// });
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log('DB connected');
+});
 
 const app = express();
 const port = 8080;
@@ -20,7 +23,10 @@ app.use(express.json());
 //// app.use(express.urlencoded({ extended: false }));
 //// app.use('/auth', authRouter);
 
+app.use('/user', userRouter);
+app.use(auth);
 app.use('/chat', chatRouter);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`running on ${port}`);
