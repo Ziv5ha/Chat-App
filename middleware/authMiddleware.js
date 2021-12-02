135c8c1867aa1b9e95e862e09b3c889d10633c39
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2021-12-02 20:55:23 +0200

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+function auth(req, res, next) {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    // res.status(401).send('Access Token Required');
+    // return;
+    next(401);
+  }
+  try {
+    const token = authorization.split(' ')[1];
+    const user = jwt.verify(token, process.env.SECRET);
+    next();
+  } catch (error) {
+    // res.status(403).send('Invalid Access Token');
+    next(403);
+  }
+}
+
+module.exports = auth;
