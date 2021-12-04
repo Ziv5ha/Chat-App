c3a7c7f9f095475e45f4fc4d43db1a85a9655452
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2021-12-04 18:41:16 +0200

    home page styling

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import ChatNav from './chatRoom/ChatNav';
 import '../styles/home-page.css';
+import LoginPage from './loginPage/LoginPage';
 
-export default function HomePage() {
+export default function HomePage({ user, setUser }) {
   return (
     <div>
-      <ChatNav />
+      <Link to='login'>Log In</Link>
+      <ChatNav user={user} />
       <div className='app'>
         This is a placeholder for a home page that showcases all the features of
         this chat app
